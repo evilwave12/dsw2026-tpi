5c82b96dc598d39c7f17606aa5bda3ab237d1c90
IanGiuliante <ian.giuliante@gmail.com>
2026-07-24 13:48:23 -0300

    avances modulo de disponibilidad: endpoints

--- Dsw2026Tpi.Application/Services/AvailabilityService.cs
+++ Dsw2026Tpi.Application/Services/AvailabilityService.cs
@@ -5,6 +5,7 @@ using Dsw2026Tpi.Domain.Entities;
 using Dsw2026Tpi.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace Dsw2026Tpi.Application.Services
@@ -42,10 +43,7 @@ namespace Dsw2026Tpi.Application.Services
 
                 var dayOfWeekNumber = MapStringToDayOfWeekNumber(dayRequest.Day);
 
-                var availability = new Availability(currentMonth, currentYear, dayOfWeekNumber, dayRequest.StartTime, dayRequest.EndTime)
-                {
-                    Doctor_Id = request.DoctorId
-                };
+                var availability = new Availability(request.DoctorId, currentMonth, currentYear, dayOfWeekNumber, dayRequest.StartTime, dayRequest.EndTime);
 
                 await _persistence.Add(availability);
 
@@ -56,10 +54,62 @@ namespace Dsw2026Tpi.Application.Services
             }
             return availabilities;
         }
-        /*public async Task UpdateAvailabilitiesAsync(AvailabilityModel.Request request)
-        {
 
-        }*/
+        public async Task UpdateAvailabilitiesAsync(AvailabilityModel.Request request)
+        {
+            var doctor = await _persistence.GetById<Doctor>(request.DoctorId);
+
+            if (doctor == null || !doctor.IsActive) 
+            { 
+                throw new EntityNotFoundException(nameof(Doctor));
+            }
+
+            var currentDate = DateTime.Now;
+            var currentMonth = (byte)currentDate.Month;
+            var currentYear = (byte)currentDate.Year;
+            var currentDay = (byte)currentDate.Day;
+
+            var currentAvailabilities = await _persistence.GetFiltered<Availability>(
+                a => a.Doctor_Id == request.DoctorId &&
+                     a.Month == currentMonth &&
+                     a.Year == currentYear
+            );
+
+            if (currentAvailabilities != null)
+            {
+                foreach (var newDayRequest in request.Days)
+                {
+
+                    var numerin = MapStringToDayOfWeekNumber(newDayRequest.Day);
+                    var existingList = currentAvailabilities?.ToList() ?? new List<Availability>();
+                    var mismoDiaExistenteDisponilidades = existingList
+                        .Where(a => a.Day_of_the_week==numerin);
+
+                    foreach (var existente in mismoDiaExistenteDisponilidades)
+                    {
+                        bool taSolapado = newDayRequest.StartTime < existente.End_time &&
+                                             newDayRequest.EndTime > existente.Start_time;
+
+                        if (taSolapado)
+                        {
+                            throw new ValidationException(); 
+                        }
+                    }
+                }
+                
+                 
+                foreach (var newDayRequest in request.Days)
+                {
+                    var numerin = MapStringToDayOfWeekNumber(newDayRequest.Day);
+                    var newAvailability = new Availability
+                    (request.DoctorId,currentMonth,currentYear,numerin,newDayRequest.StartTime,newDayRequest.EndTime);
+                    /*uwu*/
+                    await _persistence.Add(newAvailability);
+
+                    await GenerateAndSaveSlotsAsync(newAvailability, currentDate);
+                }
+            }
+        }
 
         private async Task GenerateAndSaveSlotsAsync(Availability rule, DateTime currentDate)
         {
@@ -77,10 +127,7 @@ namespace Dsw2026Tpi.Application.Services
                     {
                         var slotEnd = slotStart.AddMinutes(30);
 
-                        var newSlot = new AvailabilitySlot(rule.Id, dateToProcess, slotStart, slotEnd)
-                        {
-                            AvailabilityId = rule.Id,
-                        };
+                        var newSlot = new AvailabilitySlot(rule.Id, dateToProcess, slotStart, slotEnd);
 
                         await _persistence.Add(newSlot);
                         slotStart = slotEnd;
