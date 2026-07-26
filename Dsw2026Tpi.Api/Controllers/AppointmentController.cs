52f57d4ef62c2f3a8cdf434b1e83e98036cfc33e
IanGiuliante <ian.giuliante@gmail.com>
2026-07-25 22:48:55 -0300

    modulo de citas: endpoints GET y DELETE hechos

--- Dsw2026Tpi.Api/Controllers/AppointmentController.cs
+++ Dsw2026Tpi.Api/Controllers/AppointmentController.cs
@@ -17,6 +17,7 @@ public class AppointmentController : AppController
         _service = service;
     }
 
+
     [HttpPost]
     [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task<IActionResult> Add([FromBody] AppointmentModel.Request appointment)
@@ -24,4 +25,30 @@ public class AppointmentController : AppController
         var appointment2 = await _service.Add(appointment);
         return Ok(appointment2);
     }
+
+    //GET appointments/patient?dni={dni}
+    [HttpGet("patient")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetActiveAppointmentsByPatient([FromQuery] string dni)
+    {
+        /* ↓this es una validacion pero bueno la dejo comentada por si las dudas↓
+        if (string.IsNullOrEmpty(dni)) 
+        {
+            return BadRequest("el DNI es requerido");
+        }
+        */
+        var appointments = await _service.GetActiveAppointmentsByPatientDni(dni);
+        return Ok(appointments);
+    }
+
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> CancelAppointment(Guid id) 
+    {
+        await _service.CancelAppointment(id);
+        return NoContent();
+    }
+
 }
+
